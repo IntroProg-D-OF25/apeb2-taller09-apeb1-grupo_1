72010f86b4d624cb2a01ca12b23b85d9f07f480c
Kleiver-byte <kleiverpasaca15@gmail.com>
2024-12-17 23:17:04 -0500

    Update Ejercicio_4.java

--- Taller_9.0/src/Ejercicio_4.java
+++ Taller_9.0/src/Ejercicio_4.java
@@ -3,28 +3,28 @@ public class Ejercicio_4 {
 
     public static void main(String[] args) {
         int[] numeros = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
-        double mediaA = 0;
+        double mediaArit = 0;
         int conta1 = 0, conta2 = 0, conta3 = 0;
 
         for (int i = 0; i < numeros.length; i++) {
             mediaA += numeros[i];
         }
         mediaA /= numeros.length;
-        System.out.println("La media aritmetica es: " + mediaA);
+        System.out.println("La media aritmetica es: " + mediaArit);
 
         for (int i = 0; i < numeros.length; i++) {
-            if (numeros[i] > mediaA) {
+            if (numeros[i] > mediaArit) {
                 conta1++;
-            } else if (numeros[i] < mediaA) {
+            } else if (numeros[i] < mediaArit) {
                 conta2++;
             } else {
                 conta3++;
             }
         }
 
-        System.out.println("Numeros arriba de la media aritmética: " + conta1);
-        System.out.println("Numeros bajo la media aritmética: " + conta2);
-        System.out.println("Numeros iguales a la media aritmética: " + conta3);
+        System.out.println("Numeros arriba de la media aritmetica: " + conta1);
+        System.out.println("Numeros bajo la media aritmetica: " + conta2);
+        System.out.println("Numeros iguales a la media aritmetica: " + conta3);
     }
 }
 /**
